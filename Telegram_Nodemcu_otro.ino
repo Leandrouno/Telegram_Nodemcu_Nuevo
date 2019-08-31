96ef1843de3d60baa8fb0a7eb0c41f7282a0b7ce
Leandrouno <lmugnolo@gmail.com>
2019-08-30 23:06:44 -0300

    Update Telegram_Nodemcu_otro.ino

--- Telegram_Nodemcu_otro.ino
+++ Telegram_Nodemcu_otro.ino
@@ -1,19 +1,42 @@
 //https://github.com/shurillu/CTBot/blob/master/examples/inlineKeyboard/inlineKeyboard.ino
 
+/* Inicializamos la conexion WIFI con el Router
+char ssid[] = "Quien es Mi Idolo ?";     // el nombre de tu Red
+char password[] = "LioMessi"; // la clave de tu Red
+const char id_chat [] ="590173240";
+566903986:AAEs-5oiAimxg5HJXgbV_WuJjSoTXmTzLtM
+*/
 #include "CTBot.h"
-CTBot myBot;
 
-String ssid = "";     // REPLACE mySSID WITH YOUR WIFI SSID
-String pass = ""; // REPLACE myPassword YOUR WIFI PASSWORD, IF ANY
-String token = "";   // REPLACE myToken WITH YOUR TELEGRAM BOT TOKEN
-uint8_t led = 2;            // the onboard ESP8266 LED.    
-                            // If you have a NodeMCU you can use the BUILTIN_LED pin
-                            // (replace 2 with BUILTIN_LED)              
+#define PUERTA1_CALLBACK "Puerta1"  
+#define PUERTA2_CALLBACK "Puerta2" 
+#define PORTON_CALLBACK "Porton" 
+
+#define LED_BUILTIN D1
+#define PUERTA2     D2
+#define PUERTA1     D5
+#define PORTON      D7
+
+CTBot myBot;
+CTBotInlineKeyboard myKbd;  
+
+String ssid = "";   
+String pass = ""; 
+String token = "";  
+
 
 void setup() {
-  // initialize the Serial
+
   Serial.begin(9600);
   Serial.println("Iniciando TelegramBot...");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PUERTA1, OUTPUT);
+  pinMode(PUERTA2, OUTPUT);
+  pinMode(PORTON, OUTPUT);
+  digitalWrite(PUERTA1, HIGH);
+  digitalWrite(PUERTA2, HIGH);
+  digitalWrite(PORTON, HIGH);
 
   // connect the ESP8266 to the desired access point
   myBot.wifiConnect(ssid, pass);
@@ -22,15 +45,21 @@ void setup() {
   myBot.setTelegramToken(token);
 
   // check if all things are ok
-  if (myBot.testConnection())
-    Serial.println("\ntestConnection OK");
-  else
-    Serial.println("\ntestConnection NOK");
+  if (myBot.testConnection()){
+    Serial.println("Conectado a la Red Wifi");}
+  else{
+    Serial.println("Error al Conectar a la Red");}
 
-  // set the pin connected to the LED to act as output pin
-  pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH); // turn off the led (inverted logic!)
+ 
 
+  // add a query button to the first row of the inline keyboard
+  myKbd.addButton("Puerta 1", PUERTA1_CALLBACK, CTBotKeyboardButtonQuery);
+  // add another query button to the first row of the inline keyboard
+  myKbd.addButton("Puerta 2", PUERTA2_CALLBACK, CTBotKeyboardButtonQuery);
+  // add a new empty button row
+  myKbd.addRow();
+  // add a URL button to the second row of the inline keyboard
+  myKbd.addButton("Porton", PORTON_CALLBACK, CTBotKeyboardButtonQuery);
 }
 
 void loop() {
@@ -39,20 +68,87 @@ void loop() {
 
   // if there is an incoming message...
   if (myBot.getNewMessage(msg)) {
+    
+      if (msg.messageType == CTBotMessageText) {
+        
+          if (msg.text.equalsIgnoreCase("Teclado")) {        
+                          
+                                myBot.sendMessage(msg.sender.id, "Teclado", myKbd);
+                                                         
+                                 }
+          if (msg.text.equalsIgnoreCase("Puerta1")) {        
+                          
+                                digitalWrite(PUERTA1, LOW);
+                                delay(500);
+                                digitalWrite(PUERTA1, HIGH);
+                                Serial.println("Puerta Negra");
+                                Serial.println();
+                                myBot.endQuery(msg.callbackQueryID, "Puerta Negra Abierta", true);
+                                                         
+                                 } 
+          if (msg.text.equalsIgnoreCase("Puerta2")) {        
+                          
+                                digitalWrite(PUERTA2, LOW);
+                                delay(500);
+                                digitalWrite(PUERTA2, HIGH);
+                                Serial.println("Puerta Blanca");
+                                Serial.println();
+                                myBot.endQuery(msg.callbackQueryID, "Puerta Blanca Abierta", true);
+                                                         
+                                 }
+          if (msg.text.equalsIgnoreCase("Porton")) {        
+                          
+                                digitalWrite(PORTON, LOW);
+                                delay(500);
+                                digitalWrite(PORTON, HIGH);
+                                Serial.println("Porton");
+                                Serial.println();
+                                myBot.endQuery(msg.callbackQueryID, "Porton Accionado", true);
+                                                         
+                                 }                     
+    
+        
+        
+        
+            } 
+  
+  
+  else if (msg.messageType == CTBotMessageQuery) {
+      
 
-    if (msg.text.equalsIgnoreCase("LIGHT ON")) {              // if the received message is "LIGHT ON"...
-      digitalWrite(led, LOW);                               // turn on the LED (inverted logic!)
-      myBot.sendMessage(msg.sender.id, "Light is now ON");  // notify the sender
-    }
-    else if (msg.text.equalsIgnoreCase("LIGHT OFF")) {        // if the received message is "LIGHT OFF"...
-      digitalWrite(led, HIGH);                              // turn off the led (inverted logic!)
-      myBot.sendMessage(msg.sender.id, "Light is now OFF"); // notify the sender
-    }
-    else {                                                    // otherwise...
-      // generate the message for the sender
-      String reply;
-      reply = (String)"Welcome " + msg.sender.username + (String)". Try LIGHT ON or LIGHT OFF.";
-      myBot.sendMessage(msg.sender.id, reply);             // and send it
+      if (msg.callbackQueryData.equals(PUERTA1_CALLBACK)) {
+
+                                digitalWrite(PUERTA1, LOW);
+                                delay(500);
+                                digitalWrite(PUERTA1, HIGH);
+                                Serial.println("Puerta Negra");
+                                Serial.println();
+                                myBot.endQuery(msg.callbackQueryID, "Puerta Negra Abierta", true);
+                
+                }
+                
+    if (msg.callbackQueryData.equals(PUERTA2_CALLBACK)) {
+
+                                digitalWrite(PUERTA2, LOW);
+                                delay(500);
+                                digitalWrite(PUERTA2, HIGH);
+                                Serial.println("Puerta Blanca");
+                                Serial.println();
+                                myBot.endQuery(msg.callbackQueryID, "Puerta Blanca Abierta", true);
+                
+                }             
+   if (msg.callbackQueryData.equals(PORTON_CALLBACK)) {
+
+                                digitalWrite(PORTON, LOW);
+                                delay(500);
+                                digitalWrite(PORTON, HIGH);
+                                Serial.println("Porton Accionado");
+                                Serial.println();
+                                myBot.endQuery(msg.callbackQueryID, "Porton Accionado", true);
+                
+                }
+    
+    
     }
   }
   // wait 500 milliseconds
